82005eaf48259c86fdd711f38376072f818aa04b
Montina Ennio <ennio.montina@gmail.com>
2020-06-15 14:31:11 +0200

    Added script to add color with input

---
+++ Editor/ChangeColorWithInputEditor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(ChangeColorWithInput))]
+public class ChangeColorWithInputEditor : Editor
+{
+    ChangeColorWithInput m_changeColorWithInput;
+
+    private void OnEnable()
+    {
+        m_changeColorWithInput = (ChangeColorWithInput)target;
+    }
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        if (GUILayout.Button("Change Color"))
+            m_changeColorWithInput.ChangeColor();
+        if (GUILayout.Button("Reset Color"))
+            m_changeColorWithInput.ResetColor();
+    }
+}
---
+++ Runtime/ChangeColorWithInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColorWithInput : MonoBehaviour
+{
+    public Renderer m_targetRenderer;
+    public KeyCode m_inputKey = KeyCode.Space;
+    void Update()
+    {
+        if (Input.GetKeyDown(m_inputKey))
+            ChangeColor();
+    }
+    public void ChangeColor()
+    {
+        if (m_targetRenderer != null)
+            m_targetRenderer.sharedMaterial.color = Color.HSVToRGB(Random.Range(0, 1f), 1, 1);
+    }
+    public void ResetColor()
+    {
+        if (m_targetRenderer != null)
+            m_targetRenderer.sharedMaterial.color = new Color(1, 1, 1, 1);
+
+    }
+
+}
